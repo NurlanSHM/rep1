939b41aa6cb61604b9b843973c8a0966b6868b6a
NurlanSHM <flyxcookie@gmail.com>
2024-07-05 15:40:39 +0400

    Update car.py

--- car.py
+++ car.py
@@ -1,9 +1,13 @@
 class Car:
 
-    wheels = 4 #class variable - default
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+    
+    def drive(self):
+        print("This car is driving")
 
-    def __init__(self,make,model,year,color):
-        self.make = make        #instance variable - in constructor
-        self.model = model      #instance variable
-        self.year = year        #instance variable
-        self.color = color      #instance variable
+    def stop(self):
+        print("This car is stopped")
